b275905fb88b9fecd82cb5237117c3395eb937b9
rustam18-dev <rustam.radzabov8410@gmail.com>
2024-11-04 09:43:27 +0500

    feat: finished applicationForm

--- src/components/ApplicationForm.tsx
+++ src/components/ApplicationForm.tsx
@@ -4,7 +4,7 @@ export const ApplicationForm = () => {
   return (
     <div className="flex w-full justify-center application mt-[80px]">
       <div className="flex desktop:flex-col-reverse flex-row w-full max-w-[1230px]">
-        <div className=" w-full z-10 px-[115px] py-[80px]">
+        <div className=" w-full z-10 tablet-min:px-[115px] px-[16px] tablet-min:py-[80px] py-[32px]">
           <div className="flex flex-col justify-center items-center text-white">
             <h2 className="font-bold uppercase mb-8">Форма заявки</h2>
             <div className="grid grid-cols-1 gap-6">
@@ -37,11 +37,11 @@ export const ApplicationForm = () => {
             </div>
           </div>
         </div>
-        <div className="w-full px-[115px] py-[80px] z-10">
-          <h2 className="text-[56px] font-bold leading-[54px]">
+        <div className="w-full tablet-min:px-[115px] px-[16px] tablet-min:py-[80px] py-[32px] z-10">
+          <h2 className="tablet-min:text-[56px] text-[40px] font-bold tablet-min:leading-[54px] leading-[35px] tablet:text-center">
             получите персональное <span className="text-blue">предложение для вашего бизнеса!</span>
           </h2>
-          <p className="mt-10 leading-[22px] text-[14px]">
+          <p className="mt-10 leading-[22px] text-[14px] tablet:text-justify">
             Оставьте заявку, чтобы получить персональное предложение по комплексной
             поставке товаров для вашего бизнеса. Мы подберём оптимальные решения и
             оперативно свяжемся с вами для обсуждения деталей.
